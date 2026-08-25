8b97deb560c48c8e292f64c9367f6e14989c2f2d
SNEHIL VATTAMWAR <snehilvattamwar@SNEHILs-MacBook-Air.local>
2026-08-25 10:17:14 +0530

    fix(vercel): add createRequire to api/index.js for type module ESM package.json compatibility

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 const app = require("../artifacts/api-server/dist/app.cjs");
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
   const expressApp = (app && app.default) ? app.default : app;
   return expressApp(req, res);
-};
+}
